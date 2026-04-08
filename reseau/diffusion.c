e904ff045301b0fd7758bbf7813f475d3aa56169
tanoudiallo <tanoudialo04@gmail.com>
2026-04-09 01:04:57 +0200

    ajout de la gestion de la file d'attente

--- reseau/diffusion.c
+++ reseau/diffusion.c
@@ -11,7 +11,6 @@ NoeudAttente *file_attente = NULL;
 int diffusion_message_sens1(const char *donnee_json, int mon_socket_udp){
 
     // CREATION DE L'ENVELOPPE
-
     // enveloppe du message
     EnteteUDP enveloppe;        
     enveloppe.taille_payload = htons((uint16_t)strlen(donnee_json));        // taille
@@ -24,7 +23,6 @@ int diffusion_message_sens1(const char *donnee_json, int mon_socket_udp){
     struct sockaddr_in *dest_addr = NULL;   //
 
     /* TRANSMISSION DU PAQUET  */
-
     // Le buffer
     int TAILLE_PAQUET = strlen(donnee_json) + sizeof(EnteteUDP); 
     char *Buffer = malloc(TAILLE_PAQUET);
@@ -39,11 +37,10 @@ int diffusion_message_sens1(const char *donnee_json, int mon_socket_udp){
 
 
     // Boucle principale (sur la liste chainee)
-   
     while (dest_addr != NULL)
     {
         /* diffusion vers tous les players */
-        if(sendto(mon_socket_udp, Buffer, TAILLE_PAQUET, MSG_CONFIRM, (struct sockaddr*)dest_addr, sizeof(dest_addr)) < 0){
+        if(sendto(mon_socket_udp, Buffer, TAILLE_PAQUET, MSG_CONFIRM, (struct sockaddr*)dest_addr, sizeof(struct sockaddr_in)) < 0){
             printf("erreur-sendto");
         }
         //dest_addr = dest_addr->suivant; // avancement
@@ -78,11 +75,13 @@ int diffusion_message_sens1(const char *donnee_json, int mon_socket_udp){
 
 
 
+
+
 // Reseau->Python
 char *diffusion_message_sens2(int reseau_fd){
     /*Paquets entrant*/
-    struct sockaddr_in src_addr;
-    socklen_t addr_len = sizeof(&src_addr);
+    struct sockaddr_in addr_distant; // La boîte
+    socklen_t addr_len = sizeof(addr_distant);      
 
     // Buffer de reception du paquet
     int size_chaine_json = 2049;
@@ -94,7 +93,7 @@ char *diffusion_message_sens2(int reseau_fd){
     }
 
     // Reception
-    if(recvfrom(reseau_fd, Buffer, TAILLE_PAQUET, MSG_CONFIRM, (struct sockaddr*)&src_addr, addr_len) < 0){
+    if(recvfrom(reseau_fd, Buffer, TAILLE_PAQUET, MSG_CONFIRM, (struct sockaddr*)&addr_distant, &addr_len) < 0){
         printf("echec de reception du paquet");
         return NULL;
     }
@@ -107,11 +106,9 @@ char *diffusion_message_sens2(int reseau_fd){
     uint16_t taille_json = ntohs(enveloppe_recue->taille_payload);
 
     // Verfication du type 
-   
     switch (enveloppe_recue->type_message)
     {
-        case 0:
-            /* communication normale, envoi vers ipc */
+        case 0: /* communication normale, envoi vers ipc */
             char *donnee_json = malloc(taille_json+1);
             memcpy(donnee_json, Buffer + sizeof(EnteteUDP), taille_json);
             donnee_json[taille_json] = '\0';    // fermeture correcte de la chaine
@@ -119,23 +116,24 @@ char *diffusion_message_sens2(int reseau_fd){
             free(Buffer);   // libere le buffer
             return donnee_json;
 
-        case 1:
-            /* Nettoyage de la file d'attente */
+        case 1: /* Nettoyage de la file d'attente */
             printf("ACK recu pour le message %u\n", seq_recu);
 
             // suppression dans la file d'attente
+            uint32_t seq_confirmee = ntohl(enveloppe_recue->num_sequence);
+            supprimer_de_la_file(seq_confirmee);
 
             free(Buffer);
             return NULL;
 
-        case 2:
-            /* Mise a jour du carnet pour le ping */
+        case 2: /* detection de la deconnexion */
+            printf("[SYSTEME] Ping reçu de l'expéditeur.\n");
+            signaler_presence_a_oumar(addr_distant);  // On signale la presence du joeur
 
             free(Buffer);
-
             return NULL;
 
-    default:
+    default:    /*Message inconnu*/
         printf("[ALERTE] Type de message inconnu reçu.\n");
         free(Buffer);
         return NULL;
@@ -151,4 +149,36 @@ long get_time() {
     struct timeval temps;
     gettimeofday(&temps, NULL);
     return (temps.tv_sec * 1000) + (temps.tv_usec / 1000);
+}
+
+
+
+void supprimer_de_la_file(uint32_t seq_a_supprimer) {
+    NoeudAttente *actuel = file_attente;
+    NoeudAttente *precedent = NULL;
+
+    while (actuel != NULL) {
+        // On compare les numéros de séquence (déjà convertis en host-byte-order)
+        if (ntohl(actuel->entete.num_sequence) == seq_a_supprimer) {
+            
+            // Si c'est le premier nœud de la liste
+            if (precedent == NULL) {
+                file_attente = actuel->suivant;
+            } else {
+                // On saute le nœud actuel dans la chaîne
+                precedent->suivant = actuel->suivant;
+            }
+
+            // Libération de la mémoire
+            free(actuel);
+            printf("[FIABILITÉ] Message %u supprimé de la file d'attente.\n", seq_a_supprimer);
+            return; // On sort de la fonction
+        }
+
+        // On avance dans la liste
+        precedent = actuel;
+        actuel = actuel->suivant;
+    }
+    
+    printf("[INFO] ACK reçu pour %u, mais message déjà supprimé ou inconnu.\n", seq_a_supprimer);
 }
