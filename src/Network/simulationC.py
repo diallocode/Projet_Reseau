fbf30f68d9fb264c900c00a4e2bf2747b2a0e095
Oumar68 <eloumarwann@gmail.com>
2026-04-09 10:20:33 +0200

    modifs mineures sur simulateur reseau

--- src/Network/simulationC.py
+++ src/Network/simulationC.py
@@ -28,8 +28,8 @@ def start_mock_c():
         "type": "handshake",
         "player_id": 2,
         "units": [
-            {"id": i, "type": "Knight", "x": 50 + i, "y": 42.0, "hp": 100} 
-            for i in range(2000,2000+5) # On génère 5 unités d'un coup
+            {"id": i + 2000, "type": "Knight", "x": 50 + i, "y": 42.0, "hp": 100} 
+            for i in range(5) # On génère 5 unités d'un coup
         ]
     }
     sock.sendto(json.dumps(units_config).encode(), addr)
@@ -37,8 +37,8 @@ def start_mock_c():
 
     # 4. Simuler un mouvement simple
     time.sleep(2)
-    for i in range(2000,2005):
-        move_msg = {"type": "update","id": i, "hp": 45, "network_owner": 2, "x": 50.0+i,"y": 40.0}
+    for i in range(5):
+        move_msg = {"type": "update","id": i + 2000, "hp": 45, "network_owner": 2, "x": 50.0+i,"y": 40.0}
         sock.sendto(json.dumps(move_msg).encode(), addr)
         print("Envoyé à Python: Un mouvement d'unité")
         time.sleep(2) # Pause entre les mouvements 
