5c350285b41ef0058f145de8ebcd68a0cf70fe51
El-Hadji-Oumar-Hanne <el_hadji_oumar.hanne@insa-cvl.fr>
2026-04-09 09:59:14 +0200

    ajout de fonctionnalités de déconnions et de suppression

--- hanne_connexion/conn_multi.h
+++ hanne_connexion/conn_multi.h
@@ -2,11 +2,12 @@
 #define OUMAR_H
 
 #include <netinet/in.h> // Requis pour la structure sockaddr_in
-
+#include <time.h> // Requis pour gérer les timestamps et les timeouts²
 // Structure représentant un joueur dans le carnet d'adresses
 struct paire {
     struct sockaddr_in addr;
     int id;
+    time_t dernier_vu; // Timestamp pour gérer les timeouts
 };
 
 // ==========================================
@@ -29,4 +30,12 @@ struct paire* get_connected_peers(int *count);
  */
 void afficher_mes_ips();
 
+//fonction pour fermer la connexion 
+int close_socket(int sockfd);
+
+int remove_peer(int index);
+//qelqu'un qui se deconecte via @ip et port
+void disconnect_paire_by_addr(struct sockaddr_in addr);
+//verification des @ inactives
+int check_and_get_inactive_paire(int timeout_sec);
 #endif // OUMAR_H
